19a8d3e3ac6fdf61b9a139d222d52d82586a22f1
utshaav <utshaav583@gmail.com>
2026-03-15 01:27:01 -0400

    Implemented weekly streak

--- HabitTracker/ViewModel/HabitViewModel.cs
+++ HabitTracker/ViewModel/HabitViewModel.cs
@@ -5,4 +5,20 @@ public class HabitViewModel : Habit
 {
     public bool IsCompletedToday { get; set; }
     public List<int> LogDisplayIndicators { get; set; } = [0, 0, 0, 0, 0, 0, 0];
+    public int GetCurrentStreak()
+    {
+        int streak = 0;
+        for (int i = 0; i < LogDisplayIndicators.Count; i++)
+        {
+            if (LogDisplayIndicators[i] == 1)
+            {
+                streak++;
+            }
+            else
+            {
+                break;
+            }
+        }
+        return streak;
+    }
 }
